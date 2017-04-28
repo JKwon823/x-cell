3954068ca08ad0a9156f27c426c7c4122bb60c9a
Joseph Kwon <kwonjosepho@gmail.com>
2017-04-28 01:09:20 -0700

    add renderFormulaBar and test on table-view

--- client/js/test/table-view.test.js
+++ client/js/test/table-view.test.js
@@ -10,6 +10,23 @@ describe('table-view', () => {
 		document.documentElement.innerHTML = html;
 	});
 
+	describe('formula bar', () => {
+		it('updates FROM the value of the current cell', () => {
+			const model = new TableModel(3, 3);
+			const view = new TableView(model);
+			model.setValue({col: 2, row: 1}, '123');
+			view.init()
+
+			const formulaBarEl = document.querySelector('#formula-bar')
+			expect(formulaBarEl.value).toBe('');
+
+			const trs = document.querySelectorAll('TBODY TR');
+			trs[1].cells[2].click();
+
+			expect(formulaBarEl.value).toBe('123');
+		});
+	});
+
 	describe('table body', () => {
 		it('highlights the current cell when clicked', () => {
 			const model = new TableModel(10, 5);
